2ce28a85688526b3091d0efdf55394cbd0d31925
bkoimett <benkoimet@gmail.com>
2025-07-02 08:57:31 +0300

    Add initial implementation of people.js with sample data and logging

---
+++ modules.js
@@ -0,0 +1,4 @@
+const xyz = require('./people.js');
+//const { people } = xyz; // Importing people from people.js
+
+console.log(xyz);
---
+++ people.js
@@ -0,0 +1,10 @@
+const people = [
+    { name: 'Mario', age: 30 }, 
+    { name: 'Yoshi', age: 25 },
+    { name: 'Koimett', age: 28 },
+    { name: 'Luigi', age: 32 }
+];
+
+// const ages = people.map(person => person.age); // Extract ages from people objects
+// console.log(ages); // [30, 25, 28, 32]
+console.log(people);
