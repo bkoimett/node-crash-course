5fac810d9bc910f44fe178e41531af68c04dd86b
bkoimett <benkoimet@gmail.com>
2025-07-02 11:30:33 +0300

    Refactor: comment out file reading and writing operations in files.js

--- files.js
+++ files.js
@@ -1,22 +1,39 @@
 const fs = require('fs');
 
-fs.readFile('./docs/blog2.txt', (err, data) => { //add 'utf8' encoding if you want string output
-    // fs.readFile('./docs/blog1.txt', 'utf8', (err, data
-    if (err) {
-        console.error('Error reading file:', err);
-        return;
-    }
-    console.log('File content:', data.toString());
-})
+// fs.readFile('./docs/blog2.txt', (err, data) => { //add 'utf8' encoding if you want string output
+//     // fs.readFile('./docs/blog1.txt', 'utf8', (err, data
+//     if (err) {
+//         console.error('Error reading file:', err);
+//         return;
+//     }
+//     console.log('File content:', data.toString());
+// })
 
-// write files
+// // write files
 
-fs.writeFile('./docs/blog1.txt', 'Hello, Again', () => {
-    console.log('File was written');
-})
+// fs.writeFile('./docs/blog1.txt', 'Hello, Again', () => {
+//     console.log('File was written');
+// })
 
-fs.writeFile('./docs/blog2.txt', 'Hello, Again (now in a generated file)', () => {
-    console.log('File was written');
-})
+// fs.writeFile('./docs/blog2.txt', 'Hello, Again (now in a generated file)', () => {
+//     console.log('File was written');
+// })
 
-//
+// directories - check if folder exists and run the code if it does not exist
+if (!fs.existsSync('./assets')) {
+    fs.mkdir('./assets', (err) => {
+        if (err) {
+            console.error('Error creating directory:', err);
+            return;
+        }
+        console.log('Directory created');
+    });
+} else {
+    fs.rmdir('./assets', (err) => {
+        if (err) {
+            console.error('Error removing directory:', err);
+            return;
+        }
+        console.log('Directory removed');
+    })
+}
