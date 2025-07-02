6d46f2be723f0f3ecca8bbbb6402dc46b6428ccd
bkoimett <benkoimet@gmail.com>
2025-07-02 20:13:41 +0300

    Fix: add encoding option to read stream for blog3.txt

--- streams.js
+++ streams.js
@@ -1,8 +1,9 @@
 const fs = require ('fs');
 
-const readStream = fs.createReadStream('./docs/blog3.txt')
+const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8' });
 
 readStream.on('data', (chunk) => {
   console.log('----NEW CHUNK----');
-  console.log(chunk.toString() );
+  console.log(chunk );
 });
+
