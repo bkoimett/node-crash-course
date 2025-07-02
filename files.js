fafebc0501cc1c101cbb402cf7312aeb3712f2ab
bkoimett <benkoimet@gmail.com>
2025-07-02 11:20:21 +0300

    Refactor: update file reading in files.js to include utf8 encoding comment and improve output handling

--- files.js
+++ files.js
@@ -1,11 +1,10 @@
 const fs = require('fs');
 
-fs.readFile('./docs/blog1.txt', 'utf8', (err, data) => {
+fs.readFile('./docs/blog1.txt', (err, data) => { //add 'utf8' encoding if you want string output
+    // fs.readFile('./docs/blog1.txt', 'utf8', (err, data
     if (err) {
         console.error('Error reading file:', err);
         return;
     }
-    console.log('File content:', data);
-});
-
-console.log('Reading file completed.');
+    console.log('File content:', data.toString());
+})
