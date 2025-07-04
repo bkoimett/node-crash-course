fbdced4775d34b5f90d732f2447d758223e47c9e
bkoimett <benkoimet@gmail.com>
2025-07-04 10:42:02 +0300

    Refactor app.js: reorganize route handlers for better readability

--- js-server/app.js
+++ js-server/app.js
@@ -6,8 +6,11 @@ const app = express()
 // listen for requests
 app.listen(3000)
 
+// get handler
 app.get('/', (req, res) => {
-
-    res.send('<p>Home Page</p>')
-    
+    res.send('<p>Home Page</p>')   
 })
+
+app.get("/about", (req, res) => {
+  res.send('<p>About Page</p>');
+});
