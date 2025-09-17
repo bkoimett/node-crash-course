158f9a4f96d9d266032cb95b29304ee8e64ddc5e
bkoimett <benkoimet@gmail.com>
2025-09-17 17:16:18 +0300

    feat (app) : refactor blog routes into separate file and update app.js

--- js-server/app.js
+++ js-server/app.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
-const Blog = require('./models/blog');
-const { result } = require("lodash");
+const blogRoutes = require('./routes/blogRoutes')
+// const { result } = require("lodash");
 
 // express app initialisation
 const app = express();
@@ -23,7 +22,7 @@ app.use(express.urlencoded( {extended: true} ))
 app.use(morgan("dev"));
 
 
-// routes
+// route handlers
 app.get("/", (req, res) => {
   res.redirect('/blogs');
 });
@@ -32,63 +31,8 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
 });
 
-app.get("/create", (req, res) => {
-  res.render("create", { title: "Create a new blog" });
-});
-
-app.get('/blogs/:id', (req,res) => {
-  const id = req.params.id
-
-  Blog.findById(id)
-    .then((result) => {
-      res.render('details', { blog: result, title: 'Blog Details'})
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  //console.log(id)
-})
-
-// delete handler
-
-app.delete('/blogs/:id', (req,res) => {
-  const id = req.params.id
-
-  Blog.findByIdAndDelete(id) 
-    .then(result => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch(err =>console.log(err))
-})
-
-app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1 })
-    .then((result) => {
-      res.render('index', { title: 'All Blogs', blogs: result})
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-})
-
-// method handlers
-
-app.post('/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-  
-  blog.save()
-    .then((result) => {
-      res.redirect('/blogs')
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-})
-
-// redirect
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
-});
+// blog routes
+app.use("/blogs", blogRoutes);
 
 // 404 page - always at the bottom
 app.use((req, res) => {
